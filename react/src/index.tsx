6d6461be47e6469b36138352204cf15b7c58119d
Walter Shewmake <waltershewmake@gmail.com>
2023-02-11 13:13:02 -0700

    v1.3.0

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,4 +1,5 @@
 import { renderComponent } from './render';
 import { DataProvider, useData, useDataPoint } from './useData';
 
+export * from '@youjs/core';
 export { DataProvider, useData, useDataPoint, renderComponent };
